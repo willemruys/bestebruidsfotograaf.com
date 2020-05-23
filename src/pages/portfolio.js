609266269a10d23fb6e588a13df0b17af0762606
willemruys <wmruys@gmail.com>
2020-05-23 23:20:27 +0200

    added missing images

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React from "react";
+import Layout from "../components/layout";
+import Hero from "../components/hero/hero.component";
+import Gallery from "../components/gallery/gallery-homepage.component";
+import backgroundImage from "../images/header/Ceremony-min.jpg";
 
-import Layout from "../components/layout"
-import Hero from "../components/hero/hero.component"
-import Gallery from "../components/gallery/gallery-homepage.component"
-import backgroundImage from "../images/header/Ceremony-min.jpg"
 
 const GalleryPage = () => (
     <Layout>
-        <Hero h1="Mijn portfolio" subheader="Ik wil elk moment van jullie dag zo goed mogelijk vastleggen" background={backgroundImage}/>
-        <Gallery />
+            <Hero h1="Mijn portfolio" subheader="Ik wil elk moment van jullie dag zo goed mogelijk vastleggen" background={backgroundImage}/>
+            <Gallery />
     </Layout>    
 )
 
